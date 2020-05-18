3c6edde58788bef553ce67a7879b80b63b2b9eae
bafiam <stevebafiam@gmail.com>
2020-05-18 20:34:10 +0300

    factor when not given a string

--- capitalize.js
+++ capitalize.js
@@ -1,7 +1,12 @@
 const capitalizeString = (str) => {
-  const getFirstLetter=str.charAt(0).toUpperCase();
-  const getTheRest = str.slice(1,str.length);
-  return getFirstLetter.concat(getTheRest);
+  if (typeof str === 'string'){
+    const getFirstLetter=str.charAt(0).toUpperCase();
+    const getTheRest = str.slice(1,str.length);
+    return getFirstLetter.concat(getTheRest);
+  }
+  const error = new Error("not a string");
+  return error.message; 
+  
 }
 
 export default capitalizeString
--- capitalize.test.js
+++ capitalize.test.js
@@ -4,10 +4,10 @@ test('name is capitalized to Name', () => {
   expect(capitalizeString("name")).toBe("Name");
 });
 
-test('bafiam is capitalized to Bafiam', () => {
-  expect(capitalizeString("bafiam")).toBe("Bafiam");
-});
-
 test('bafiam is capitalized to cant be bafiam', () => {
   expect(capitalizeString("bafiam")).not.toBe("bafiam");
+});
+
+test('return not a string given a number', () => {
+  expect(capitalizeString(1)).toBe("not a string");
 });
